bcf65266e4d3546d35cbc660dcafd11b5ec44f89
Alberthinks <117525757+Alberthinks@users.noreply.github.com>
2023-04-19 18:21:18 +0200

    Update showLocandina.php

--- app/showLocandina.php
+++ app/showLocandina.php
@@ -8,7 +8,4 @@ $url = "../evento/locandine/".$link_foto_video;
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <link rel="stylesheet" href="../css/default_phone.css" type="text/css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0">
-<header>
-    <a class="material-symbols-outlined" onclick="history.back()">arrow_back</a>
-</header>
 <img src="<?php echo $url; ?>" alt="Locandina" style="width: 100%; margin-top: 90px;">
